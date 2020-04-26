5249a244d9798844dbc3470b31f9345b977000dc
Andrei Bucurei <bucurei.andrei@gmail.com>
2020-04-26 14:12:36 +0300

    Added test for RealmResults max and maxDate. Gradle update.

--- app/src/androidTest/java/dev/kodorebi/realmtypesafe/RealmResultsTests.kt
+++ app/src/androidTest/java/dev/kodorebi/realmtypesafe/RealmResultsTests.kt
@@ -3,9 +3,13 @@ package dev.kodorebi.realmtypesafe
 import android.util.Log
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import dev.kodorebi.realmtypesafe.models.TestModel
+import dev.kodorebi.realmtypesafe.results.max
+import dev.kodorebi.realmtypesafe.results.maxDate
 import io.realm.Realm
 import io.realm.RealmConfiguration
 import io.realm.kotlin.where
+import org.junit.Assert
 import org.junit.BeforeClass
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -36,6 +40,37 @@ class RealmResultsTests {
 
     @Test
     fun max() {
+        val results = realm.where<TestModel>().findAll()
 
+        val pd2 = results.max(TestModel::pDouble)
+        val pd1 = results.max("pDouble")?.toDouble()
+
+        val od2 = results.max(TestModel::oDouble)
+        val od1 = results.max("oDouble")?.toDouble()
+
+        val pf2 = results.max(TestModel::pFloat)
+        val pf1 = results.max("pFloat")?.toFloat()
+
+        val of2 = results.max(TestModel::oFloat)
+        val of1 = results.max("oFloat")?.toFloat()
+
+        assert(pd2 == pd1)
+        assert(od2 == od1)
+        assert(pf2 == pf1)
+        assert(of2 == of1)
+    }
+
+    @Test
+    fun maxDate() {
+        val results = realm.where<TestModel>().findAll()
+
+        val pd2 = results.maxDate(TestModel::pDate)
+        val pd1 = results.maxDate("pDate")
+
+        val od2 = results.maxDate(TestModel::oDate)
+        val od1 = results.maxDate("oDate")
+
+        assert(pd2 == pd1)
+        assert(od2 == od1)
     }
 }
